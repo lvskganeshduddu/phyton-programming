fceed081dfac4ca0b3f165cddc2b1c9579a72b2d
lvskganeshduddu <42571282+lvskganeshduddu@users.noreply.github.com>
2019-02-20 10:20:32 +0530

    Delete 1.py

--- 1.py
+++
@@ -1,9 +0,0 @@
-print("Enter '.5' for exit.");
-ch = input("Enter any character: ");
-if ch == '.5':
-    exit();
-else:
-    if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
-    	print(ch, "is an alphabet.");
-    else:
-    	print(ch, "is not an alphabet.");
